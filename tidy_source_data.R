71e237b39b1ca51f295a7869beabaddc7475ec15
Wenbo Wang <wenbo@live.unc.edu>
2020-09-07 02:41:30 -0400

    Added tidysource data.

--- tidy_source_data.R
+++ tidy_source_data.R
@@ -16,10 +16,22 @@ names <- c('patient_type' ,'sex', 'pneumonia', 'intubed','pregnancy','diabetes',
            'obesity', 'renal_chronic','tobacco', 'contact_other_covid','covid_res', 'icu', 'dependent');
 covid[,names] <- lapply(covid[,names] , factor);
 
+
+covid[,"entry_date"] <- as.Date(covid[,"entry_date"], format = "%d-%m-%Y");
+covid[,"date_symptoms"] <- as.Date(covid[,"date_symptoms"], format = "%d-%m-%Y");
+covid$waitTime <- as.vector(difftime(covid$entry_date, covid$date_symptoms, units='days'))
+
 #plots
 p <- ggplot(covid, aes(dependent, age, fill=patient_type)) +
       geom_violin() + scale_fill_discrete(name="Patient Type",labels=c("Outpatient", "Inpatient")) +
       xlab("Mortality") + ylab("Age")
 ggsave("figures/Patient_Type_Age.png",plot=p);
 
+#attempt at a separate chart
+q <- ggplot(covid, aes(x = waitTime, color = dependent)) + geom_histogram(alpha = 0.25, binwidth = 15) +
+    scale_fill_discrete(name="Mortality",labels=c("No", "Yes")) +
+    xlab("Age") + xlim(0,20)
+#ggsave("figures/Patient_Type_Age.png",plot=p);
+
 write.csv(covid, "derived_data/covid.csv");
+write.csv(covid, "derived_data/covid_dup.csv")
