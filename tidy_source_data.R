fae59987e5f4a7dbc70c7c32ba88f2c0b2e70df6
Wenbo Wang <wenbo@live.unc.edu>
2020-09-07 01:30:38 -0400

    Added more tasks.

--- tidy_source_data.R
+++ tidy_source_data.R
@@ -1,7 +1,25 @@
 library(tidyverse)
+library(ggplot2)
 
-covid <- read_csv("./source_data/covid.csv");
+cov <- read.csv("./source_data/covid.csv");
 
-## Clean, etc. data
+#remove duplicate id rows
+covid <- cov[!duplicated(cov$id),];
+covid_dup <- cov[duplicated(cov$id),];
+
+#make variable for dependent variable
+covid$dependent <- ifelse(as.numeric(substring(covid$date_died,1,2)) >31, 0, 1);
+
+#Factorize variables
+names <- c('patient_type' ,'sex', 'pneumonia', 'intubed','pregnancy','diabetes',
+           'copd', 'asthma', 'inmsupr','hypertension','other_disease','cardiovascular',
+           'obesity', 'renal_chronic','tobacco', 'contact_other_covid','covid_res', 'icu', 'dependent');
+covid[,names] <- lapply(covid[,names] , factor);
+
+#plots
+p <- ggplot(covid, aes(dependent, age, fill=patient_type)) +
+      geom_violin() + scale_fill_discrete(name="Patient Type",labels=c("Outpatient", "Inpatient")) +
+      xlab("Mortality") + ylab("Age")
+ggsave("figures/Patient_Type_Age.png",plot=p);
 
 write.csv(covid, "derived_data/covid.csv");
