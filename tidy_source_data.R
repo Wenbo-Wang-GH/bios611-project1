f1b094242b645293e3b59ec0021f2b8a0ac68201
Wenbo Wang <wenbo@live.unc.edu>
2020-09-07 03:04:51 -0400

    Modified code for axis.

--- tidy_source_data.R
+++ tidy_source_data.R
@@ -30,7 +30,7 @@ ggsave("figures/Patient_Type_Age.png",plot=p);
 #attempt at a separate chart
 q <- ggplot(covid, aes(x = waitTime, fill = dependent)) + geom_histogram(aes(y = ..density..),alpha = 0.50, binwidth = 1) +
   scale_fill_discrete(name="Mortality",labels=c("No", "Yes")) +
-  xlab("Age") + xlim(0,20) +geom_vline(xintercept = 3.639876, size = 0.5,alpha = 0.25, colour = "red", linetype = "solid") + 
+  xlab("Wait Time") + ylab("Density of Cases") + xlim(0,20) +geom_vline(xintercept = 3.639876, size = 0.5,alpha = 0.25, colour = "red", linetype = "solid") + 
   geom_vline(xintercept = 4.046071, size = 0.5,alpha = 0.5, colour = "turquoise", linetype = "solid") 
 ggsave("figures/Wait_Time.png",plot=q);
 
