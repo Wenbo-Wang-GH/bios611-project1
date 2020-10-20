fe501441b18fd4ef9c6a0fb15e7540fb0c5c9760
Wenbo Wang <wenbo@live.unc.edu>
2020-10-19 20:41:48 -0400

    Added more details to charts.

--- patient_type_vs_age.R
+++ patient_type_vs_age.R
@@ -5,7 +5,7 @@ covid <- readRDS("derived_data/covid.csv");
 
 #plots
 p <- ggplot(covid, aes(dependent, age, fill=patient_type)) +
-  geom_violin() + scale_fill_discrete(name="Patient Type",labels=c("Outpatient", "Inpatient")) +
+  geom_violin() + scale_fill_discrete(name="Patient Type",labels=c("Out-patient", "In-patient")) +
   xlab("Mortality") + ylab("Age")
 
 ggsave("figures/Patient_Type_Age.png",plot=p)
--- wait_time.R
+++ wait_time.R
@@ -5,7 +5,7 @@ covid <- readRDS("derived_data/covid.csv");
 
 q <- ggplot(covid, aes(x = waitTime, fill = dependent)) + geom_histogram(aes(y = ..density..),alpha = 0.50, binwidth = 1) +
   scale_fill_discrete(name="Mortality",labels=c("No", "Yes")) +
-  xlab("Wait Time (Days)") + ylab("Density of Cases") + xlim(0,20) +geom_vline(xintercept = 3.639876, size = 0.5,alpha = 0.25, colour = "red", linetype = "solid") + 
-  geom_vline(xintercept = 4.046071, size = 0.5,alpha = 0.5, colour = "turquoise", linetype = "solid")
+  xlab("Wait Time (Days)") + ylab("Density of Cases") + xlim(0,20) +geom_vline(xintercept = 3.185575, size = 0.5,alpha = 0.25, colour = "red", linetype = "solid") + 
+  geom_vline(xintercept = 3.330703, size = 0.5,alpha = 0.5, colour = "turquoise", linetype = "solid")
 
 ggsave("figures/Wait_Time.png",plot=q)
